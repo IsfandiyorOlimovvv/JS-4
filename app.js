0d56adfaded59874e4c9b67c8bcf30d9ce301884
IsfandiyorOlimovvv <shoxagaming@gmail.com>
2024-10-16 21:09:53 +0500

    js4-dars

---
+++ app.js
@@ -0,0 +1,38 @@
+// let age = prompt("Iltimos yoshingizni kiriting:");
+
+// if (isNaN(age)) {
+//     console.log("Invalid input please enter a number.");
+// } else {
+//     age = Number(age);
+//     if (age < 18) {
+//         console.log("you are a minor");
+//     } else if (age >= 18 && age <= 60) {
+//         console.log("you are an adult");
+//     } else if (age > 60) {
+//         console.log("uou are a senior cideizen.");
+//     }
+// }
+
+
+
+
+
+// let choice = prompt("ultimos, 1 dan 4 gacha bo'lgan raqamni tanlang:1: Breakfast2: Lunch3: Dinner4: Snack");
+
+// switch (choice) {
+//     case '1':
+//         console.log("you chose breakfast.");
+//         break;
+//     case '2':
+//         console.log("you chose lunch");
+//         break;
+//     case '3':
+//         console.log("you chose dinner");
+//         break;
+//     case '4':
+//         console.log("you chose snack");
+//         break;
+//     default:
+//         console.log("invalid choice. Please choose a number between 1 and 4");
+//         break;
+// }
